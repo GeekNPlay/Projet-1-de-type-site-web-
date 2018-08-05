8b20f10238d7e8ae4346fd2f9bfa2c46cbd524e2
TenebrisLuxNoctis <tenebrisluxnoctis@laposte.net>
2018-08-05 18:06:19 +0200

    ajout standard header

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
-<html>
+<!DOCTYPE html>
+<html lang="FR" >
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <title>MasterSnakou | Site</title>
+</head>
 
 <style>
     
