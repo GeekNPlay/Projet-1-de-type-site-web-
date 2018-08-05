7fe699807ddabafcc094342b519d3427aea914d5
TenebrisLuxNoctis <tenebrisluxnoctis@laposte.net>
2018-08-05 17:52:00 +0200

    Page principalité

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+
+<style>
+    
+    .videoBG{
+	position: fixed; 
+	z-index: -1;
+	top: 0px; 
+	left: 0px; 
+	height: 100%; 
+	width: 100%; 
+	opacity: 1; 
+	overflow: hidden;
+}
+
+</style>
+
+    <div class="videoBG">
+		<video poster="" autoplay="autoplay" src="https://mastersnakou.fr/theme/frontend/img/video.webm" loop ></video>
+	</div>
+
+</html>
